34c15e4a510d668a9ddbb47f4d77c278ce1aa71e
Muhammad Usman Said <ussaid150@gmail.com>
2025-05-21 17:05:30 +0500

    Delete PYTHON/Lab1 directory

--- PYTHON/Lab1/Question3.py
+++
@@ -1,8 +0,0 @@
-apples = int(input("Enter the number of Apples: "))
-childs = int(input("Enter the number of Students: "))
-
-everyStudentGets = int(apples / childs)
-print("Every student should get " + str(everyStudentGets))
-
-remainedInBasket = apples % childs
-print("Apples remained in the Basket: " + str(remainedInBasket))
--- PYTHON/Lab1/Question5.py
+++
@@ -1,6 +0,0 @@
-totalMinutes = int(input("Enter the minutes: "))
-
-hours = int(totalMinutes / 60)
-minutes = int(totalMinutes % 60)
-
-print("Now the time is: " + str(hours) + ":" + str(minutes))
